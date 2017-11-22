b43c7ea313615874e40b1535523ab267fe92d9ff
Tatiana Kuzenkova <tatiana.kuzenkova@gentlecode.com>
2017-11-22 18:21:49 +0200

    enhancements

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp'),
     watch = require('gulp-watch'),
+    $ = require('cheerio'),
     filerigger = require('gulp-file-include'),
     sass = require('gulp-sass'),
     sassOptions = {
@@ -59,11 +60,17 @@ const gulp = require('gulp'),
             return data;
         });
     },
+    write = (file, text) => {
+        return fs.writeFile(file, text, 'utf8', (err) => {
+            if (err) console.error(err)
+        });
+    },
+
     makeWeb = (req) => {
         const hhToc = get(`${hhc}`);
         const header = get(`src/assets/partials/webHeader.html`);
         const footer = get(`src/assets/partials/webFooter.html`);
-        let html = hhToc
+        let toc = hhToc
             .substring(hhToc.indexOf('<UL>'))
             .replace(/<OBJECT type=\"text\/sitemap\">/g, '<a')
             .replace(/<param name=\"Name\" value=\"(.+?)\">/g, ' title="$1"')
@@ -72,13 +79,32 @@ const gulp = require('gulp'),
             .replace(/\"\s*\n\t*\s*/g, '" ')
             .replace(/<a\s*\n*/g, '<a ')
             .replace(/<a title="(.+?)"(.+?)>/g, '<a title="$1"$2>$1');
-        html = html.substring(0, html.indexOf('</BODY>'));
-        html = `
+        toc = toc.substring(0, toc.indexOf('</BODY>'));
+        let index = toc
+            .replace(/<UL>/g,'')
+            .replace(/<\/UL>/g,'')
+            .replace(/<\/LI>/,'')
+            .replace(/<!--/g,'')
+            .replace(/-->/,'')
+            .replace(',','');
+        toc = $(toc).attr("id","toc");
+        const compare = (a1, a2) => {
+            var t1 = $(a1).text(), t2 = $(a2).text();
+            return t1 > t2 ? 1 : (t1 < t2 ? -1 : 0);
+        };
+        const sortList = (list) => {
+            const lis = list.split('<LI>');
+            return lis.sort(compare);
+        };
+        index = `<UL id="index">${sortList(index).join('<LI>')}</UL>`;
+
+        const html = `
             ${header}
-            ${html}
+            ${toc}
+            ${index}
             ${footer}
         `;
-        fs.writeFile('index.html', html, 'utf8', (err) => { if (err) console.log(err) });
+        write('index.html', html);
     };
 
 
